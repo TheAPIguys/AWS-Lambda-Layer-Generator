6a1e93ed9bdf4c81467781db0239edb243fd7b24
Daniel Office Computer <daniel@rfe.co.nz>
2023-07-06 13:38:51 +1200

    modified:   main.py 	deleted:    main2.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 import shutil
-import zipfile
 import subprocess
+import time
 
 # Prompt user for dependencies
 dependencies = []
+print('Enter the dependencies you want to install in the Lambda layer. Please make sure that ')
+
 while True:
-    dependency = input("Enter a dependency (or 'done' to finish): ")
-    if dependency.lower() == "done":
+    count_dependencies = len(dependencies)
+    if count_dependencies != 0:
+        print(f'Current dependencies: {dependencies}')
+    dependency = input("Enter a dependency (or 'n' to finish): ")
+    if dependency.lower() == "n":
         break
     dependencies.append(dependency)
 
@@ -16,19 +21,46 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Change the working directory to the script's directory
 os.chdir(script_dir)
+os.mkdir('nodejs')
+#subprocess.check_call('mkdir nodejs', shell=True, cwd=script_dir)
+new_dir = os.chdir(os.path.join(script_dir, 'nodejs'))
 
+
+
+os.system('cls')
+print('Starting creating project...')
 # Initialize Node.js environment
-subprocess.check_call('npm init -y', shell=True, cwd=script_dir)
+subprocess.check_call('npm init -y', shell=True, cwd=new_dir)
 
+os.system('cls')
+print('Installing dependencies...')
 # Install the dependencies
-subprocess.check_call(['npm', 'install'] + dependencies, shell=True, cwd=script_dir)
+subprocess.check_call(['npm', 'install'] + dependencies, shell=True, cwd=new_dir)
+
+os.system('cls')
+print('Finish dependencies...')
+print('Creating Zip file...')
 
 # Create a zip file
-zip_filename = 'dependencies.zip'
-shutil.make_archive(os.path.join(script_dir, 'node_modules'), 'zip', 'node_modules')
+zip_filename = f'layer-{dependencies}'
+
+nj_folder= os.path.join(script_dir, 'nodejs')
+os.remove(os.path.join(nj_folder, 'package-lock.json'))
+os.remove(os.path.join(nj_folder, 'package.json'))
+
+shutil.make_archive(os.path.join(script_dir, zip_filename), 'zip',new_dir)
+
+os.system('cls')
+
 
 print('Zip file created successfully!')
+print('Removing unnecessary files...')
 
 # Remove unnecessary files
-shutil.rmtree(os.path.join(script_dir, 'node_modules'))
-os.remove(os.path.join(script_dir, 'package-lock.json'))
+time.sleep(1)
+os.chdir(script_dir)
+shutil.rmtree(os.path.join(script_dir, 'nodejs'))
+
+
+os.system('cls')
+print(f'All done!, your layer has been zipped and is ready to be uploaded to AWS Lambda. Zip file: {zip_filename}')
